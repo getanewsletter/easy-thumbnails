55cbf0cbb8fc54499ba83a71e4fe8db0a49bf01c
eng. Ilian Iliev <ilian@i-n-i.org>
2015-12-02 15:02:37 +0100

    Commenting pil

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django>=1.4.2',
-        'pillow',
+        #  'pillow',  # commenting pillow, we will install it manually
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
